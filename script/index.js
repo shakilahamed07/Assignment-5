6c647546b4be35aaa499432d2c520a654558681d
shakilahamed07 <shakil200607@gmail.com>
2025-03-02 02:24:20 +0600

    add task manegment script

---
+++ script/index.js
@@ -0,0 +1,63 @@
+// blog page open
+document.getElementById('blog')
+    .addEventListener('click', function(){
+        window.location.href="blog.html"
+    });
+
+// date
+window.addEventListener('load', function(){
+    let date = Date();
+    let newDate = date.split(" ")
+    for(let i = 0; i<4; i++){
+        }
+    document.getElementById('date').innerHTML=`
+    <p>${newDate[0]} ,</p>
+    <p class="text-xl font-bold text-color">${newDate[1]} ${newDate[2]} ${newDate[3]} </p>
+    `
+})
+
+// Task management operation
+const taskButtons = document.querySelectorAll('.completed-btn');
+let allClickBtn = [];
+let taskAssigned = getIdConvatTaxt('task-assigned');
+let taskNumber = getIdConvatTaxt("task-number");
+for(const taskBtn of taskButtons){
+    let Button = taskBtn;
+    Button.addEventListener('click', function(event){
+        alert("Board update Successfully")
+
+        taskNumber = taskNumber + 1;
+        taskAssigned = taskAssigned - 1;
+        document.getElementById('task-number').innerText = taskNumber;
+        document.getElementById('task-assigned').innerText = taskAssigned;
+
+        let historyContainer = document.getElementById("history");
+        let title = event.target.parentNode.parentNode.parentNode.querySelector("h1").innerText;
+        let now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+        const seconds = now.getSeconds(); 
+
+        let div  = document.createElement('div');
+        div.innerHTML = `
+        <p class="p-3 bg-secondary-color rounded-lg mb-3 j">You have Complete The Task ${title} at ${hours}:${minutes}:${seconds}</p>
+        `;
+        historyContainer.append(div);
+
+        const clickBtn = event.target.setAttribute("disabled", false);
+        allClickBtn.push(clickBtn);
+        if(allClickBtn.length == 6){
+            alert("Congrats!!! You have completed all current task");
+        }
+    })
+}
+
+// clear history
+for(let i = 0; i<=6; i++){
+    document.getElementById('history-btn')
+    .addEventListener('click', function(){
+        let history = document.getElementById('history');
+        history.querySelector('p').remove()
+    })
+}
+
---
+++ script/utility.js
@@ -0,0 +1,11 @@
+
+function getQuarySclectorText(id){
+    let  value = document.querySelector(id).innerText;
+    return value;
+}
+
+function getIdConvatTaxt(id){
+    let value = document.getElementById(id).innerText;
+    let convatInt = parseInt(value);
+    return convatInt;
+}
